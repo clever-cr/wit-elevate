960691803172b7f21c2daf3a2375d70a947bc6d2
clever-cr <cleverumuhuza@gmail.com>
2023-12-02 21:06:11 +0200

    fix issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import "@mui/x-date-pickers/dist/DateRangePicker/DateRangePicker.css";
-// import "@mui/x-date-pickers/dist/DatePicker/DatePicker.css";
-// import "@mui/x-date-pickers/dist/DateTimePicker/DateTimePicker.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+import tsconfigPaths from 'vite-tsconfig-paths';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsconfigPaths()],
+});
